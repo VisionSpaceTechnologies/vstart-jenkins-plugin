ef5e66ec197f58e8e55e988d1303e6d47b354d56
Pedro Marinho <pedromarinho.t@gmail.com>
2015-07-22 16:17:49 +0100

    URL validation working in the Jenkins global configurations menu

--- Vstart-Plugin/src/main/java/com/visionspace/vstart/plugin/VSPluginBuilder.java
+++ Vstart-Plugin/src/main/java/com/visionspace/vstart/plugin/VSPluginBuilder.java
@@ -13,11 +13,16 @@ import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.QueryParameter;
 import com.visionspace.vstart.api.Vstart;
+import hudson.model.FreeStyleProject;
 import hudson.util.ListBoxModel;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import org.apache.commons.validator.UrlValidator;
 
 
@@ -32,14 +37,16 @@ public class VSPluginBuilder extends Builder {
     private final String vstAddress;
     private final String vstUser;
     private final String vstPass;
+    private final Vstart vst;
     
     // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
     @DataBoundConstructor
-    public VSPluginBuilder(String name, String address, String user, String pass) {
+    public VSPluginBuilder(String vstAddress, String vstUser, String vstPass) throws URISyntaxException {
        
-        this.vstAddress = address;
-        this.vstUser = user;
-        this.vstPass = pass;
+        this.vstAddress = vstAddress;
+        this.vstUser = vstUser;
+        this.vstPass = vstPass;
+        this.vst = this.login();
     }
 
     public String getVstAddress() {
@@ -54,6 +61,10 @@ public class VSPluginBuilder extends Builder {
         return vstPass;
     }
 
+    public Vstart getVst(){
+        return vst;
+    }
+    
     @Override
     public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
         // This is where you 'build' the project.
@@ -69,19 +80,20 @@ public class VSPluginBuilder extends Builder {
      * @return Vstart API object
      * @throws URISyntaxException 
      */
-    public Vstart login() throws URISyntaxException{
-        Vstart vst = new Vstart(vstAddress, vstUser, vstPass);
-        return vst;
+    private Vstart login() throws URISyntaxException{
+        Vstart v = new Vstart(vstAddress, vstUser, vstPass);
+        return v;
     };
 
     
     public ListBoxModel doFillProjectTypeItems(Vstart v) throws URISyntaxException {
             ListBoxModel items = new ListBoxModel();
-            v = this.login();
-            
-            for (int i = 0; i < v.listProjects().length(); i++) {
-                items.add(v.listProjects().getString(i));
+                        
+            for(int j = 0; j < vst.listProjects().length(); j++){
+                String project =  vst.listProjects().getJSONObject(j).toString();
+                items.add(project);
             }
+            
             return items;
         }
     
@@ -96,32 +108,65 @@ public class VSPluginBuilder extends Builder {
     
     @Extension // This indicates to Jenkins that this is an implementation of an extension point.
     public static class Descriptor extends BuildStepDescriptor<Builder> {
-        
+               
         @Override
         public String getDisplayName()
         {
             return "Execute VSTART tasks.";
         };
         
-        public FormValidation doCheckAddress(@QueryParameter String address)
-                throws IOException, ServletException {
-            UrlValidator urlVal = new UrlValidator();
+        /**
+         * Tests the validity of an URL
+         * @param url 
+         * @return true if valid, false if not valid
+         */
+        public boolean isValidURL(String nUrl){
+            URL u = null;
             
-            if (urlVal.isValid(address))
-                return FormValidation.ok();
+            try {
+                u = new URL(nUrl);
+            } catch (MalformedURLException e) {
+                return false;
+            }
+            try {
+                u.toURI();
+            } catch (URISyntaxException z) {
+                return false;
+            }
+            return true;
+        }
+        
+        public FormValidation doCheckAddress(@QueryParameter("vstAddress") final String address)
+                throws IOException, ServletException {
+                                 
+            if(this.isValidURL(address))
+                return FormValidation.ok("Success!");
             else return FormValidation.error("Please insert a valid URL!");
             
         }
         
+             
+        public FormValidation doCheckLogin(@QueryParameter String address , @QueryParameter String user, @QueryParameter String pass) 
+                throws URISyntaxException
+        {
+            try {   
+                Vstart v = new Vstart(address, user, pass);
+                return FormValidation.ok();
+            } catch(URISyntaxException e) {    
+                return FormValidation.error("Login failed!");
+            }
+        }
+        
         @Override
         public boolean isApplicable(Class<? extends AbstractProject> jobType) {
-            return true;
+            return FreeStyleProject.class.isAssignableFrom(jobType);
         };
 
         @Override
         public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
             // To persist global configuration information,
             // set that to properties and call save().
+            req.bindJSON(this, formData);
             
             // ^Can also use req.bindJSON(this, formData);
             //  (easier when there are many fields; need set* methods for this, like setUseFrench)
