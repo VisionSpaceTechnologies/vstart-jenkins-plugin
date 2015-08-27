ce583896b9f8874395691b2e94ea4dd9f7456f75
Pedro Marinho <pedromarinho.t@gmail.com>
2015-08-27 10:00:54 +0100

    Migrated HTML report to the Recorder class, changed the logic for the .json file on workspace

--- Vstart-Plugin/src/main/java/com/visionspace/vstart/plugin/VSPluginRecorder.java
+++ Vstart-Plugin/src/main/java/com/visionspace/vstart/plugin/VSPluginRecorder.java
@@ -6,6 +6,7 @@
 package com.visionspace.vstart.plugin;
 
 import hudson.Extension;
+import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
@@ -17,6 +18,12 @@ import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Publisher;
 import hudson.tasks.Recorder;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONArray;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
@@ -26,44 +33,71 @@ import org.kohsuke.stapler.DataBoundConstructor;
 public class VSPluginRecorder extends Recorder {
 
     @DataBoundConstructor
-    public VSPluginRecorder(){
-    
+    public VSPluginRecorder() {
+
     }
-    
+
     @Override
     public BuildStepMonitor getRequiredMonitorService() {
         return BuildStepMonitor.BUILD;
     }
-    
+
     @Override
-    public Action getProjectAction(AbstractProject<?, ?> project){
+    public Action getProjectAction(AbstractProject<?, ?> project) {
         return new VSPluginProjectAction(project);
     }
-    
+
     @Override
-     public boolean perform(AbstractBuild build, Launcher launcher, final BuildListener listener){
-     
+    public boolean perform(AbstractBuild build, Launcher launcher, final BuildListener listener) {
+
         Result buildResult = build.getResult();
-                
+
         if (!Result.SUCCESS.equals(buildResult)) {
             // Don't process for unsuccessful builds
             listener.getLogger().println("Build status is not SUCCESS (" + build.getResult().toString() + ").");
             return false;
         }
-                
+
+        try {
+
+            //DO REPORT HERE
+            FilePath jPath = new FilePath(build.getWorkspace(), build.getWorkspace() + "/VSTART_JSON");
+
+            if (!jPath.exists()) {
+                return false;
+            }
+
+            String filePath = jPath + "/VSTART_JSON_"
+                    + build.getId() + ".json";
+            
+            //read .json file
+            String content = new String(Files.readAllBytes(Paths.get(filePath)));
+            JSONArray reports = new JSONArray(content);
+            
+            //Generate html report
+            VSPluginHtmlWriter htmlWriter = new VSPluginHtmlWriter();
+            boolean reportResult = htmlWriter.doHtmlReport(build, reports);
+            
+            return reportResult;
+        } catch (IOException ex) {
+            Logger.getLogger(VSPluginRecorder.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        } catch (InterruptedException ex) {
+            Logger.getLogger(VSPluginRecorder.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    @Override
+    public BuildStepDescriptor getDescriptor() {
+        return super.getDescriptor();
+    }
+
+    @Override
+    public boolean needsToRunAfterFinalized() {
         return true;
-     }
-     
-    @Override
-     public BuildStepDescriptor getDescriptor(){
-         return super.getDescriptor();
-     }
-     
-    @Override
-     public boolean needsToRunAfterFinalized(){
-         return true;
-     }
-     
+    }
+
     @Extension // This indicates to Jenkins that this is an implementation of an extension point.
     public static class DescriptorRecorder extends BuildStepDescriptor<Publisher> {
 
@@ -76,6 +110,6 @@ public class VSPluginRecorder extends Recorder {
         public String getDisplayName() {
             return "VSTART Publisher.";
         }
-    
+
     }
 }
