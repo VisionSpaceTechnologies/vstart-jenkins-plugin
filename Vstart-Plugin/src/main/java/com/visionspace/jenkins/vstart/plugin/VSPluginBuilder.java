52fcfd23b18336f987780853405fc3afce2e5288
Mario Ulisses Costa <ucosta@visionspace.com>
2015-09-18 18:43:16 +0100

    add LICENSE, pom with maven.visionspace.com and srcs with license headers

--- Vstart-Plugin/src/main/java/com/visionspace/jenkins/vstart/plugin/VSPluginBuilder.java
+++ Vstart-Plugin/src/main/java/com/visionspace/jenkins/vstart/plugin/VSPluginBuilder.java
@@ -1,4 +1,20 @@
-package com.visionspace.vstart.plugin;
+/* 
+ * Copyright (C) 2015 VisionSpace Technologies, Lda.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.visionspace.jenkins.vstart.plugin;
 
 import com.cloudbees.plugins.credentials.CredentialsProvider;
 import com.cloudbees.plugins.credentials.common.StandardUsernameCredentials;
